c034740717e951a8fbf93182d7c668ce8c2f1c6c
marlow-fm <mmcwaters@ucsd.edu>
2026-06-05 00:36:53 -0700

    Clean up next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    turbo: {
-      enabled: false,
-    },
-  },
-};
-
+const nextConfig = {};
 module.exports = nextConfig;
