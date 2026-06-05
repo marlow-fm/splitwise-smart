6b04fb5f3263cad0469f765bb7ad0e301fd483ab
marlow-fm <mmcwaters@ucsd.edu>
2026-06-05 00:10:49 -0700

    Disable Turbopack in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    turbo: {
+      enabled: false,
+    },
+  },
+};
+
 module.exports = nextConfig;
